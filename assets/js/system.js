76d44995f75f53a07ee2c9c8ccd66087a487305a
Ansari <zansari765@gmail.com>
2022-09-28 20:32:31 +0530

    zee-system-resp

--- assets/js/system.js
+++ assets/js/system.js
@@ -9,16 +9,38 @@
 
   function performAction(e){
     if(e.type === 'load'){
-      if(!uploadDesc.classList.contains('active')){
+      if(uploadDesc!==null && !uploadDesc.classList.contains('active')){
         uploadDesc.classList.add('active');
         overlay.classList.add('active');
       }
     }
     else if(e.type === 'click' && e.target === overlay){
-      if(uploadDesc.classList.contains('active')){
+      if(uploadDesc!==null && uploadDesc.classList.contains('active')){
         uploadDesc.classList.remove('active');
         overlay.classList.remove('active');
       }
     }
   }
+})();
+
+//dropdown
+(()=>{
+  const dropTrigger = document.querySelector('.main_header .grid2 .profile'),
+  dropBox = document.querySelector('.main_header .grid2 .dropdown');
+
+  dropTrigger.addEventListener('click',()=>{
+      dropBox.classList.toggle('active');
+  })
+})();
+
+// mobile slider
+(()=>{
+  const sliderrow = document.querySelector('.system-div .row.no-wrap'),
+  stepper = document.querySelectorAll('.system-div .stepper');
+
+  stepper.forEach((elem,ind)=>{
+    if(elem.querySelector('.progress2.active')){
+      sliderrow.style.transform = "translateX(" + (ind * -53) + "%)";
+    }
+  })
 })();
