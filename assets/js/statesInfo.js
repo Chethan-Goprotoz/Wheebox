25e36c4b48e1bbbe6898fa759e17e9c4fb37cb42
Vamsi-Goprotoz <vamsi@goprotoz.com>
2022-11-12 21:16:21 +0530

    added

---
+++ assets/js/statesInfo.js
@@ -0,0 +1,216 @@
+var maptotal = document.getElementById("maptotal")
+var mappass = document.getElementById("mappass")
+var mapfail = document.getElementById("mapfail")
+var mapname = document.getElementById("mapname")
+
+
+function andhraPradesh(){
+    mapname.textContent = "Andhra Pradesh"
+    maptotal.textContent = "2300"
+    mappass.textContent = "2000"
+    mapfail.textContent = "300"
+}
+
+function jammu(){
+    mapname.textContent = "Jammu & Kashmir"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function himachal(){
+    mapname.textContent = "Himachal Pradesh"
+    maptotal.textContent = "2300"
+    mappass.textContent = "2000"
+    mapfail.textContent = "300"
+}
+
+function punjab(){
+    mapname.textContent = "Punjab"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function Uttarakhand(){
+    mapname.textContent = "Uttarakhand"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+
+function Haryana(){
+    mapname.textContent = "Haryana"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function Uttar(){
+    mapname.textContent = "Uttar Pradesh"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function Rajasthan(){
+    mapname.textContent = "Rajasthan"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function Madhya(){
+    mapname.textContent = "Madhya Pradesh"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function Gujarat(){
+    mapname.textContent = "Gujarat"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function Maharashtra(){
+    mapname.textContent = "Maharashtra"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function Goa(){
+    mapname.textContent = "Goa"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function Karnataka(){
+    mapname.textContent = "Karnataka"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function Kerala(){
+    mapname.textContent = "Kerala"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function TamilNadu(){
+    mapname.textContent = "Tamil Nadu"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function Telangana(){
+    mapname.textContent = "Telangana"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function Odisha(){
+    mapname.textContent = "Odisha"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+
+function Chhattisgarh(){
+    mapname.textContent = "Chhattisgarh"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+
+function Jharkhand(){
+    mapname.textContent = "Jharkhand"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+
+function Bihar(){
+    mapname.textContent = "Bihar"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function WestBengal(){
+    mapname.textContent = "West Bengal"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+
+function Sikkim(){
+    mapname.textContent = "Sikkim"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+
+function Assam(){
+    mapname.textContent = "Assam"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function Meghalaya(){
+    mapname.textContent = "Meghalaya"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function Tripura(){
+    mapname.textContent = "Tripura"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function ArunachalPradesh(){
+    mapname.textContent = "Arunachal Pradesh"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function Nagaland(){
+    mapname.textContent = "Nagaland"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+function Manipur(){
+    mapname.textContent = "Manipur"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
+
+function Mizoram(){
+    mapname.textContent = "Mizoram"
+    maptotal.textContent = "1300"
+    mappass.textContent = "1000"
+    mapfail.textContent = "300"
+}
+
